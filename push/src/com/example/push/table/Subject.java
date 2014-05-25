65ef3ca6c665ffa5861ca658280a6bf6a87be38f
kyoungrak0121 <kyoungrak0121@gmail.com>
2014-05-26 01:23:26 +0900

    database + prof

---
+++ push/src/com/example/push/table/Subject.java
@@ -0,0 +1,14 @@
+package com.example.push.table;
+
+public class Subject {
+	
+	private String subjectNum ;
+	private String subjectName; 
+	
+	public Subject(String num , String name) {
+		// TODO Auto-generated constructor stub
+		this.subjectName=name;
+		this.subjectNum=num;
+	}
+
+}
